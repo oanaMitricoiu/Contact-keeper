3b594f3f820e9337cb69bb5cbce56a0102cb1145
oana-iu <oana.mitri01@gmail.com>
2024-04-15 15:02:04 +0300

    add log in

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
+
+//Connect Database
+connectDB();
+
+//Init middleware
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => {
     res.send({ msg: "Welcome to the contact keeper API" });
