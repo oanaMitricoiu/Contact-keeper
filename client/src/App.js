7865a0dae834c737cfb89b5fd2d4c656efad35e2
oana-iu <oana.mitri01@gmail.com>
2024-04-16 21:06:04 +0300

    add register and login

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,33 @@ import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import ContactState from "./context/contact/ContactState";
+import AuthState from "./context/auth/AuthState";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
     return (
-        <ContactState>
-            <Router>
-                <Fragment>
-                    <Navbar />
-                    <div className="container">
-                        <Routes>
-                            <Route path="/" element={<Home />} />
-                            <Route path="/about" element={<About />} />
-                        </Routes>
-                    </div>
-                </Fragment>
-            </Router>
-        </ContactState>
+        <AuthState>
+            <ContactState>
+                <Router>
+                    <Fragment>
+                        <Navbar />
+                        <div className="container">
+                            <Routes>
+                                <Route path="/" element={<Home />} />
+                                <Route path="/about" element={<About />} />
+                                <Route
+                                    path="/register"
+                                    element={<Register />}
+                                />
+                                <Route path="/login" element={<Login />} />
+                            </Routes>
+                        </div>
+                    </Fragment>
+                </Router>
+            </ContactState>
+        </AuthState>
     );
 };
 
